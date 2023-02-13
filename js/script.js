92705ef7f37e03040849ba6c2f3a698ec2aaa5b3
pivkTone04 <118509605+pivkTone04@users.noreply.github.com>
2023-02-13 14:14:45 +0100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -41,4 +41,25 @@ function temnoModraBarva(x){
 	document.getElementById(x).classList.toggle("spremeniBarvoTemnoModro");
 	
 }
+var c = document.getElementById("myCanvas");
+var ctx = c.getContext("2d");
+
+
+drawShape(ctx, 1, -200);
+function drawShape(ctx, xoff, yoff) {
+	  ctx.beginPath();
+	  ctx.moveTo(348 + xoff, 373 + yoff);
+	  ctx.bezierCurveTo(337 + xoff, 374 + yoff, 317 + xoff, 389 + yoff, 312 + xoff, 395 + yoff);
+	  ctx.bezierCurveTo(302 + xoff, 407 + yoff, 299 + xoff, 414 + yoff, 298 + xoff, 421 + yoff);
+	  ctx.bezierCurveTo(296 + xoff, 436 + yoff, 302 + xoff, 452 + yoff, 331 + xoff, 454 + yoff);
+	  ctx.bezierCurveTo(351 + xoff, 455 + yoff, 425 + xoff, 439 + yoff, 438 + xoff, 434 + yoff);
+	  ctx.bezierCurveTo(446 + xoff, 431 + yoff, 530 + xoff, 404 + yoff, 536 + xoff, 401 + yoff);
+	  ctx.bezierCurveTo(551 + xoff, 394 + yoff, 525 + xoff, 386 + yoff, 514 + xoff, 390 + yoff);
+	  ctx.bezierCurveTo(493 + xoff, 397 + yoff, 442 + xoff, 411 + yoff, 438 + xoff, 412 + yoff);
+	  ctx.bezierCurveTo(419 + xoff, 417 + yoff, 379 + xoff, 421 + yoff, 363 + xoff, 420 + yoff);
+	  ctx.bezierCurveTo(326 + xoff, 417 + yoff, 346 + xoff, 372 + yoff, 349 + xoff, 373 + yoff);
+	  ctx.stroke();
+	  ctx.fillStyle= "black";
+	  ctx.fill();
+}
 
